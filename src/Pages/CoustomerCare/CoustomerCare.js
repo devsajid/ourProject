530f598a878f47604474a63e9601be89297b2e08
devsajid <74415469+devsajid@users.noreply.github.com>
2022-09-01 19:48:33 +0600

    file setup

---
+++ src/Pages/CoustomerCare/CoustomerCare.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoustomerCare = () => {
+  return <div></div>;
+};
+
+export default CoustomerCare;
---
+++ src/Pages/HowToShop/HowToShop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HowToShop = () => {
+  return <div></div>;
+};
+
+export default HowToShop;
